db6def759e15e1a2332c1755b8aca16ddf2969d9
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2020-10-18 22:31:40 -0700

    build: Remove grunt. Convert SASS to CSS. Remove package.json

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Copyright (c) 2014 Lokesh Dhakar
+HuddleTogether.com
+
+Copyright (c) Lokesh Dhakar
--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-window.addEventListener('load', function (event) {
-  positionHeader();
-});
-
 window.addEventListener('resize', function (event) {
   // Resize Processing canvas once user is finished resizing window
 	if (this.resizeUI) {
@@ -9,15 +5,6 @@ window.addEventListener('resize', function (event) {
 	}
 
   this.resizeUI = setTimeout(function() {
-    positionHeader();
     Processing.getInstanceById('canvas').resize();
   }, 200);  
 });
-
-function positionHeader() {
-  // Position logo and link in vertical center of viewport
-  var header          = document.getElementById('header');
-  var viewportHeight  = window.innerHeight;
-  var headerTopMargin = ((viewportHeight / 2) - (header.offsetHeight / 2));
-  header.style.margin = headerTopMargin + "px 0 0 0";
-}
